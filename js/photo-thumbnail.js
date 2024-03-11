ba1ccb559b392766b5861f06b748dc011a7848f7
Anastasia Smirnova <smirnovnana@yandex.ru>
2024-03-11 19:39:05 +0300

    Добавляет деструктуризацию

--- js/photo-thumbnail.js
+++ js/photo-thumbnail.js
@@ -8,12 +8,12 @@ const photosDescription = getPhotosDescription();
 
 const picturesFragment = document.createDocumentFragment();
 
-photosDescription.forEach((photo) => {
+photosDescription.forEach(({url, description, likes, comments}) => {
   const pictureTemplateClone = pictureTemplate.cloneNode(true);
-  pictureTemplateClone.querySelector('.picture__img').src = photo.url;
-  pictureTemplateClone.querySelector('.picture__img').alt = photo.description;
-  pictureTemplateClone.querySelector('.picture__likes').textContent = photo.likes;
-  pictureTemplateClone.querySelector('.picture__comments').textContent = photo.comments.length;
+  pictureTemplateClone.querySelector('.picture__img').src = url;
+  pictureTemplateClone.querySelector('.picture__img').alt = description;
+  pictureTemplateClone.querySelector('.picture__likes').textContent = likes;
+  pictureTemplateClone.querySelector('.picture__comments').textContent = comments.length;
   picturesFragment.appendChild(pictureTemplateClone);
 });
 
